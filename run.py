0ef18482ca0492532f50fd875c19628d8d041309
lumeeeee <vachemerega@gmail.com>
2025-12-27 17:16:17 +0300

    runpychange

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
 import threading
+
 from app import create_app
-from app.db import init_db
+from app.models import create_tables
 from app.monitor import monitor_loop
 
 if __name__ == "__main__":
-    init_db()
-    threading.Thread(target=monitor_loop, daemon=True).start()
+    create_tables()
+
+    threading.Thread(
+        target=monitor_loop,
+        daemon=True
+    ).start()
 
     app = create_app()
     app.run(host="0.0.0.0", port=8000)
